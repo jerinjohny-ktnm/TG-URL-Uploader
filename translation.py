e80a03eba8002dbba332e5ce8cec46add8024053
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2020-11-12 11:13:52 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -9,7 +9,7 @@ This is a Telegram URL Upload Bot!
 <b>Made With 💓 by @imjerin</b>"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
+    UPGRADE_TEXT = "<b>👉 Currently this is free, no need to upgrade🙅.. </b>  /help for Details"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
