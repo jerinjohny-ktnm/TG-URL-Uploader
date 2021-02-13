0f388b4591a8e09da26d7cc1af18a68bc38f5372
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2021-02-14 00:42:08 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -40,8 +40,22 @@ async def help_user(bot, update):
         text=Translation.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("HELP", callback_data="help_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "CREATOR", url="https://t.me/imjerij")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+         )
+     except:
+         pass
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
